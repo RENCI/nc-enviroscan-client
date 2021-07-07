16df496ca2f94896966bdc92c439a6872f7d25ee
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-07-07 12:17:23 -0400

    file updates

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Image, Grid, Button, List } from "semantic-ui-react"
 
 import water from '../../images/home/Hero_Image_Water_300x-100.jpg'
+import triangle from '../../images/home/triangle_mid-page_300x-8.png'
 
 function Home() {
   return (
     <div className="page-contain">
-      <Grid padded='vertically' centered>
+      <Grid padded='vertically' centered className="home-pg">
         <Grid.Row>
           <Grid.Column>
             <Image src={water} alt='waves' fluid />
           </Grid.Column>
+          <Image src={triangle} fluid />
+
         </Grid.Row>
         <Grid.Row>
           <Grid.Column width={9}>
@@ -27,6 +30,7 @@ function Home() {
               <List.Item as='a'>Environmental Indicators</List.Item>
               <List.Item as='a'>Sociodemographic Indicators</List.Item>
               <List.Item as='a'>Health Effects</List.Item>
+              <List.Item as='a'>Environmental Justice</List.Item>
             </List>
           </Grid.Column>
           <Grid.Column width={3}>
