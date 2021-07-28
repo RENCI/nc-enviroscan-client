2fc0c0e049f8d5f79986e61199e558c81236f083
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-07-28 01:41:14 -0400

    home images positioned; links and text updated

--- src/components/pages/main/Home.js
+++ src/components/pages/main/Home.js
@@ -1,22 +1,34 @@
 import React from "react";
-import { Image, Grid, Button, List } from "semantic-ui-react"
+import { Image, Grid, Button, List, Header } from "semantic-ui-react"
+import { Link } from "@reach/router"
 
-import water from '../../../images/home/Hero_Image_Water_300x-100.jpg'
+import mapImg from "../../../images/home/Map_Tool_Img.jpg"
+import drinkWater from "../../../images/home/Man_Drinking_Water.png"
+import plants from "../../../images/home/Home_Plants.png"
+
 
 function Home() {
   return (
     <div>
       <Grid padded='vertically' centered>
-        <Grid.Row>
-          <Grid.Column width={16}>
-            <Image src={water} alt='waves' fluid />
-          </Grid.Column>
+        <Grid.Row id="hero-container">
+          <Header id="hero-title">
+            North Carolina ENVIROSCAN helps communities increase awareness of key environmental and societal factors that can impact health.
+          </Header>
+          <Button color="blue" content="Launch Mapping Tool" href="https://enviroscan-map.renci.org/" target="_blank" id="hero-btn" />
+          <Image src={mapImg} alt="NC Enviroscan Mapping Tool" size="medium" id="hero-map" />
+          <Image src={drinkWater} alt="Man drinking water" size="medium" id="man-img" />
+          <span id="enviroscan-info">
+              <p>NC ENVIROSCAN allows users to visualize trends across environmental contaminants, sociodemographic information, environmental justice indicators, and health outcomes throughout North Carolina.</p>
+              <p>These indicators can be viewed individually, side-by-side on two maps, or overlayed onto the same map, allowing users to identify new patterns relevant to their concerns.</p>
+              <p>Community members that want to learn more about their local health risks and potential solutions to these risks may benefit from this tool, along with policy makers, government agencies, clinicians and scientists.</p>
+          </span>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column width={9}>
             <Button.Group fluid size='large' widths='4'>
-              <Button id="data-btn">Explore Data</Button>
-              <Button id="facts-btn">Location Search</Button>
+              <Button id="data-btn">About Our Data</Button>
+              <Button id="facts-btn">Quick Facts</Button>
               <Button id="resource-btn">Community Resources</Button>
             </Button.Group>
           </Grid.Column>
@@ -24,14 +36,14 @@ function Home() {
         <Grid.Row divided>
           <Grid.Column width={3}>
             <List bulleted>
-              <List.Item as='a'>Environmental Indicators</List.Item>
-              <List.Item as='a'>Sociodemographic Indicators</List.Item>
-              <List.Item as='a'>Health Outcomes</List.Item>
+              <List.Item as={Link} to="environmental-indicators/water">Environmental Indicators: Water</List.Item>
+              <List.Item as={Link} to="sociodemographic-indicators">Sociodemographic Indicators</List.Item>
+              <List.Item as={Link} to="health-outcomes">Health outcomes</List.Item>
             </List>
           </Grid.Column>
           <Grid.Column width={3}>
             <List bulleted>
-              <List.Item as='a'>Tool access</List.Item>
+              <List.Item as='a' href="https://enviroscan-map.renci.org/" target="_blank">Mapping Tool</List.Item>
               <List.Item as='a'>Search by location</List.Item>
             </List>
           </Grid.Column>
@@ -42,6 +54,7 @@ function Home() {
             </List>
           </Grid.Column>
         </Grid.Row>
+        <Image src={plants} />
       </Grid>
     </div>
   )
