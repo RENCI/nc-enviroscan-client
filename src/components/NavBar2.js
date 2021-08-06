d04eef54eb4c342a2b218f9fac9042592b04f217
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-08-06 13:57:40 -0400

    adjust images in hero container; change top bar to div

--- src/components/NavBar2.js
+++ src/components/NavBar2.js
@@ -5,14 +5,13 @@ import { NavbarData } from "./NavbarData";
 import { Menu, Image } from "semantic-ui-react";
 
 import logo from "../images/enviroscan_logo_300x.png";
-import topBar from "../images/shapes/top_bar_300x-8.png";
 import NavLinks from "./NavLinks";
 
 const NavBar2 = () => {
 
   return (
     <div className="header-bar">
-      <img src={topBar} alt="" id="nav-topbar" />
+      <div id="top-bar"></div>
       <Menu borderless widths="5" id="navbar">
         {/* Logo */}
         <Menu.Item header as={Link} to="/">
