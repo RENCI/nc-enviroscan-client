e59212e9d8949146c99d5eaaa0387b8553e860b4
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-08-19 04:09:36 -0400

    tab updates; rename icons

--- src/components/pages/layout/TabbedView.js
+++ src/components/pages/layout/TabbedView.js
@@ -1,9 +1,44 @@
 import React from 'react'
-import { Tab } from 'semantic-ui-react'
+import { Tab, Item } from 'semantic-ui-react'
+
+import As from "../../../images/contaminants/Arsenic_icon.png"
+import Cd from "../../../images/contaminants/Cadmium_icon.png"
+import Pb from "../../../images/contaminants/Lead_icon.png"
+import Mn from "../../../images/contaminants/Manganese_icon.png"
 
 const TabbedView = props => {
     const panes = [
-        {menuItem: 1}, {menuItem: 2}, {menuItem: 3}, {menuItem: 4}]
+        {
+            menuItem: "Arsenic", render: () => (
+                <Item>
+                    <Item.Image src={As} size="small" />
+                </Item>
+            )
+        }, 
+        {
+            menuItem: "Cadmium", render: () => (
+                <Item>
+                    <Item.Image src={Cd} size="small" />
+                </Item>
+            )
+        }, 
+        {
+            menuItem: "Lead",
+            render: () => (
+                <Item>
+                    <Item.Image src={Pb} size="small" />
+                </Item>
+            )
+        }, 
+        {
+            menuItem: "Manganese",
+            render: () => (
+                <Item>
+                    <Item.Image src={Mn} size="small" />
+                </Item>
+            )
+        }
+    ]
 
     return (
         <div>
