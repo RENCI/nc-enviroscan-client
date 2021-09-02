9648ae9c9d89603f800aacc683459250a52182c4
Matt Watson <mattwat@gmail.com>
2021-09-02 16:08:18 -0400

    migrate three-panel menu to mui cards

--- components/panel-menu/panel-menu.js
+++ components/panel-menu/panel-menu.js
@@ -1,12 +1,53 @@
 import { Link } from '../link'
 import styles from './panel-menu.module.css'
+import { Card, CardContent, CardHeader, Container, Grid, List, ListItem, makeStyles, Typography, useMediaQuery } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  grid: {
+    marginTop: '6rem',
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    width: '66%',
+    gap: '1rem',
+  },
+  compactGrid: {
+    marginTop: '6rem',
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    width: '90%',
+  },
+  card: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    overflow: 'hidden',
+  },
+  cardHeader: {
+    flex: '0 0 75px',
+    maxHeight: '75px',
+    textAlign: 'center',
+  },
+  cardContent: {
+    flex: '1 0 200px',
+    backgroundColor: '#f3f6f9',
+    padding: 0,
+  },
+  cardLink: {
+    width: '100%',
+    padding: '0.25rem 0',
+  },
+})
 
 const panels = [
   {
     title: 'About Our Data',
     colors: ['#fda67a', '#f15b25'],
     items: [
-      { text: 'Environment Indicators', path: '/environmental-indicators' },
+      { text: 'Environmental Indicators', path: '/environmental-indicators' },
       { text: 'Sociodemographic indicators', path: '/sociodemographic-indicators' },
       { text: 'Health outcomes', path: '/health-outcomes' },
     ],
@@ -30,28 +71,37 @@ const panels = [
 ]
 
 export const PanelMenu = () => {
+  const classes = useStyles()
+  const compact = useMediaQuery('(max-width: 882px)')
+
   return (
-    <div className={ styles.container }>
+    <Grid spacing={ 2 } className={ compact ? classes.compactGrid : classes.grid }>
       {
         panels.map(panel => (
-          <div key={ panel.title } className={ styles.panel }>
-            <h2 className={ styles.panelHeader } style={{ backgroundImage: `linear-gradient(to bottom, ${ panel.colors[0] }, ${ panel.colors[1] })` }}>
-              { panel.title }
-            </h2>
-            <div className={ styles.panelBody }>
-              <ul className={ styles.panelItemList }>
-                {
-                  panel.items.map(item => (
-                    <li key={ item.text } className={ styles.panelItemListItem }>
-                      <Link to={ item.path }>{ item.text }</Link>
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
-          </div>
+          <Grid item key={ panel.title }>
+            <Card className={ classes.card } xl={ 6 } elevation={ 1 }>
+              <CardHeader
+                className={ classes.cardHeader }
+                style={{ backgroundImage: `linear-gradient(to bottom, ${ panel.colors[0] }, ${ panel.colors[1] })` }}
+                title={ panel.title }
+              />
+              <CardContent className={ classes.cardContent }>
+                <List>
+                  {
+                    panel.items.map(item => (
+                      <ListItem button key={ `${ panel.title }_${ item.text }` }>
+                        <Link to={ item.path } className={ classes.cardLink }>
+                          <Typography>{ item.text }</Typography>
+                        </Link>
+                      </ListItem>
+                    ))
+                  }
+                </List>
+              </CardContent>
+            </Card>
+          </Grid>
         ))
       }
-    </div>
+    </Grid>
   )
 }
