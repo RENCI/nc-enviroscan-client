d5728f047e6033cd687813ad5f75833db8f1fb4b
Matt Watson <mattwat@gmail.com>
2021-11-08 12:03:00 -0500

    add about view emphasis and links

--- pages/about.js
+++ pages/about.js
@@ -5,19 +5,19 @@ import { Markdown } from '../components/markdown'
 const content = {
   title: 'About NC Enviroscan',
   description: `
-NC ENVIROSCAN was developed to provide a user-friendly tool that increases North
-Carolina communities’ awareness of key environmental indicators of concern, key
-social stressors of concern and health outcome information, such as that related
-to COVID-19 cases. It is also designed to be a tool for NC public health
-researchers to identify communities at greatest public health risk to inform
+NC ENVIROSCAN was developed to provide a **user-friendly** tool that increases **North
+Carolina communities’** awareness of **key environmental indicators** of concern, **key
+social stressors** of concern and **health outcome** information, such as that related
+to COVID-19 cases. It is also designed to be a tool for **NC public health
+researchers** to identify communities at greatest public health risk to inform
 research to action. This project will help researchers understand environmental
 contributions to COVID-19 and other health outcomes.
 
 ### Background
 
-NC ENVIROSCAN is a project of the UNC Superfund Research Program (UNC SRP) and
-UNC Institute of Environmental Health Solutions (UNC IEHS) in partnership with
-the Renaissance Computing Institute (RENCI). 
+NC ENVIROSCAN is a project of the [UNC Superfund Research Program](https://sph.unc.edu/superfund-pages/srp/) (UNC SRP) and
+[UNC Institute of Environmental Health Solutions](https://sph.unc.edu/iehs/institute-for-environmental-health-solutions/) (UNC IEHS) in partnership with
+the [Renaissance Computing Institute](https://renci.org) (RENCI). 
 
 The idea for NC ENVIROSCAN was born out of a creativity hub brainstorming
 session by a team of researchers from the UNC Gillings School of Global
@@ -35,30 +35,34 @@ Earth Data Science Group uses leading edge cyberinfrastructure and advanced
 environmental modeling to enable research discovery and practical applications.
 
 The development team is particularly grateful for the review and feedback
-provided by our external advisors and their constituents, including the NC
+provided by our external advisors and their constituents, including the **NC
 Department of Health & Human Services’ Occupational and Environmental Epidemiology
-Branch, and UNC SRP Public Health Action Committee (PHAC) community partners
-Clean Water for North Carolina, Winyah Rivers Alliance & the Lumber Riverkeeper,
+Branch**, and [UNC SRP Public Health Action Committee](https://sph.unc.edu/superfund-pages/public-health-action-committee-members/) (PHAC) community partners
+**Clean Water for North Carolina**, **Winyah Rivers Alliance**, & the **Lumber Riverkeeper**,
 and other SRP PHAC members.
 
 We would like to acknowledge the hard work of individuals on the initial
 development team including:
 
 ### UNC SRP
-Rebecca Fry, Center Director
-Andrew George, Community Engagement Coordinator
-Kathleen Gray, Community Engagement Core Leader
-Lauren Eaves, SRP Trainee
-Julia Rager, Data Management & Analysis Core Leader
-Megan Rodgers, Research Translation Assistant
-Sarah Yelton, Research Translation Coordinator
-RENCI 
+
+- Rebecca Fry, Center Director
+- Andrew George, Community Engagement Coordinator
+- Kathleen Gray, Community Engagement Core Leader
+- Lauren Eaves, SRP Trainee
+- Julia Rager, Data Management & Analysis Core Leader
+- Megan Rodgers, Research Translation Assistant
+- Sarah Yelton, Research Translation Coordinator
+
+#### RENCI 
+
 - Chris Lenhardt, Domain Scientist
 - Marcus Anderson, Visual Arts Specialist
-- Laurie Bernadel, Web Developer
 - Jim McManus, Research Software Developer
 - Matt Watson, Web Architect
-- External Advisors
+
+#### External Advisors
+
 - Jefferson Currie, Winyah Rivers Alliance
 - Kim Gaetz, NC Department of Health & Human Services
 - Virginia Guidry, NC Department of Health & Human Services
