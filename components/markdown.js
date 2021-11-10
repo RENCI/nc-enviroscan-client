10608da5f7b4f43eb5cfd7a6f708f579f62902fd
Suejin <kim.suejin.20@gmail.com>
2021-11-10 13:20:46 -0500

    updated styling for paragraphs and h4s on markdown file

--- components/markdown.js
+++ components/markdown.js
@@ -15,6 +15,16 @@ const useStyles = makeStyles({
     },
 
   },
+  paragraph: {
+    margin: '30px',
+    lineHeight: 1.5,
+  },
+  heading4: {
+    fontSize: '16px',
+    margin: '16px',
+    lineHeight: 1.5,
+    fontStyle: 'italic'
+  },
 })
 
 
@@ -22,14 +32,30 @@ export const Markdown = ({ src }) => {
   const classes = useStyles()
 
   const componentMap = useMemo(() => ({
-    h1: function Heading1({ node, ...props }) { return <Typography variant="h1" { ...props } /> },
-    h2: function Heading2({ node, ...props }) { return <Typography variant="h2" { ...props } /> },
-    h3: function Heading3({ node, ...props }) { return <Typography variant="h3" { ...props } /> },
-    h4: function Heading4({ node, ...props }) { return <Typography variant="h4" { ...props } /> },
-    h5: function Heading5({ node, ...props }) { return <Typography variant="h5" { ...props } /> },
-    h6: function Heading6({ node, ...props }) { return <Typography variant="h6" { ...props } /> },
-     a: function Anchor({ node, href, children, ...props }) { return <Link to={ href } { ...props }>{ children }</Link> },
-     p: function Paragraph({ node, ...props}) { return <Typography paragraph { ...props } /> },
+    h1: function Heading1({ node, ...props }) { 
+        return <Typography variant="h1" { ...props } /> 
+    },
+    h2: function Heading2({ node, ...props }) { 
+      return <Typography variant="h2" { ...props } /> 
+    },
+    h3: function Heading3({ node, ...props }) { 
+      return <Typography variant="h3" { ...props } /> 
+    },
+    h4: function Heading4({ node, ...props }) { 
+      return <Typography variant="h4" className={classes.heading4} { ...props } /> 
+    },
+    h5: function Heading5({ node, ...props }) { 
+      return <Typography variant="h5" { ...props } /> 
+    },
+    h6: function Heading6({ node, ...props }) { 
+      return <Typography variant="h6" { ...props } /> 
+    },
+    a: function Anchor({ node, href, children, ...props }) { 
+       return <Link to={ href } { ...props }>{ children }</Link> 
+    },
+    p: function Paragraph({ node, ...props}) { 
+       return <Typography paragraph className={classes.paragraph} { ...props } /> 
+    },
   }), [])
 
   return (
